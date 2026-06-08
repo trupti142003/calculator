76831787b5a1e51226b68ed882ce051d91a05598
trupti142003 <142775954+trupti142003@users.noreply.github.com>
2026-06-08 14:47:12 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname));
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
